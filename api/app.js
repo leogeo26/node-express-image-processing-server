a8c7686ca4b6d40b78222270e5b9be9fd0c68a78
Georgescu Cornel <cgeorgescu1984@gmail.com>
2021-09-03 13:58:34 +0300

    Completed module

--- api/app.js
+++ api/app.js
@@ -3,10 +3,10 @@ const path    = require('path');
 
 const app = express();
 
-const pathToIndex = path.resolve( __dirname ,'../client/index.html');
+const pathToIndex = path.resolve(__dirname ,'../client/index.html');
 
 app.use('/*', (request , response) => {
     response.sendFile(pathToIndex);
 });
 
-module.exports.app = app;
+module.exports = app;
