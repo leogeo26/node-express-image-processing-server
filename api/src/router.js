7b7b58c5497a1bc2b28150f57bf3185705d814a0
Georgescu Cornel <cgeorgescu1984@gmail.com>
2021-09-03 14:52:57 +0300

    Completed module

--- api/src/router.js
+++ api/src/router.js
@@ -3,35 +3,34 @@ const multer     = require('multer');
 
 const router = Router();
 
-function filename (request, file, callback){
+const filename = (request, file, callback) => {
     callback(null, file.originalname);
 }
 
 const storage = multer.diskStorage({
     destination : 'api/uploads/',
-    filename : filename,
+    filename,
 });
 
-function fileFilter(request, file, callback) {
-    /*
+const fileFilter = (request, file, callback) => {
     if(file.mimetype !== 'image/png'){
         request.fileValidationError = 'Wrong file type';
         callback(null, false, new Error('Wrong file type'));
     }else{
-        callback(null, true );
-    }*/
+        callback(null, true);
+    }
 }
 
 const upload = multer({
-    fileFilter : fileFilter(),
-    storage : storage,
+    fileFilter,
+    storage,
 });
 
 router.post('/upload', upload.single('photo'), (request , response)=>{
     if(request.fileValidationError){
-        response.status(400).json({error : request.fileValidationError});
+        return response.status(400).json({error : request.fileValidationError});
     } else {
-        response.status(201).json({success: true});
+        return response.status(201).json({success: true});
     }
 });
 
