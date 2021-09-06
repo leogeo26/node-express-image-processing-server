6016592d3bc34ccc15e58b1bc99cfe1f75171673
Georgescu Cornel <cgeorgescu1984@gmail.com>
2021-09-06 11:41:38 +0300

    Completed module

--- api/src/imageProcessor.js
+++ api/src/imageProcessor.js
@@ -2,9 +2,16 @@ const path = require('path');
 const {Worker , isMainThread } = require('worker_threads');
 
 const pathToResizeWorker     = path.resolve(__dirname, 'resizeWorker.js');
-const pathToMonochromeWorker = path.resolve(__dirname, 'monochromeWorker.js')
+const pathToMonochromeWorker = path.resolve(__dirname, 'monochromeWorker.js');
+
+function uploadPathResolver(filename){
+    return path.resolve(__dirname, '../uploads',filename);
+}
 
 const imageProcessor = (filename ) => {
+
+    const resizeWorkerFinished = false;
+    const monochromeWorkerFinished = false;
 
     const sourcePath = uploadPathResolver(filename);
     const resizedDestination = uploadPathResolver('resized-'+filename);
@@ -12,8 +19,6 @@ const imageProcessor = (filename ) => {
 
     return new Promise((resolve, reject)=>{
         if(isMainThread){
-            reject(new Error('not on main thread'));
-        }else{
             try {
                 const resizeWorker = new Worker(pathToResizeWorker, {
                     workerData : {
@@ -27,17 +32,63 @@ const imageProcessor = (filename ) => {
                         source : sourcePath,
                         destination : monochromeDestination 
                     }
-                })
+                });
+
+                resizeWorker.on('message', (message)=>{
+                    
+                    resizeWorkerFinished = true;
+
+                    if(monochromeWorkerFinished){
+                        resolve('resizeWorker finished processing');
+                    }
+                });
+
+                resizeWorker.on('error', (error)=>{
+                    reject(new Error(error.message));
+                });
+
+                resizeWorker.on('exit', (code)=>{
+                    if(code !== 0){
+                        reject(new Error('Exited with status code ' + code));
+                    }
+                });
+
+
+                monochromeWorker.on('message', (message)=>{
+                    monochromeWorkerFinished = true;
+
+                    if(resizeWorkerFinished) {
+                        resolve('monochromeWorker finished processing');
+                    }
+
+                });
+
+                monochromeWorker.on('error', (error)=>{
+                    reject(new Error(error.message));
+                });
+
+                monochromeWorker.on('exit', (code)=>{
+                    if(code !== 0){
+                        reject(new Error('Exited with status code ' + code));
+                    }
+                });
+
+
+
+
+
+
             } catch (error) {
                 reject(error);
             }
-            resolve();   
+        }else{
+            reject(new Error('not on main thread'));             
         }
+
+        resolve();  
     });
 }
 
-function uploadPathResolver(filename){
-    return path.resolve(__dirname, '../uploads',filename);
-}
+
 
 module.exports = imageProcessor;
--- api/src/router.js
+++ api/src/router.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const multer     = require('multer');
-const path       = require('path');
+const path           = require('path');
+const imageProcessor = require('./imageProcessor');
 
 const photoPath = path.resolve(__dirname, '../../client/photo-viewer.html');
 
@@ -29,8 +30,14 @@ const upload = multer({
     storage,
 });
 
-router.post('/upload', upload.single('photo'), (request , response)=>{
+router.post('/upload', upload.single('photo'), async (request , response)=>{
     if(request.fileValidationError){
+        try {
+            await imageProcessor(request.file.filename);
+        } catch (error) {
+
+        }
+
         return response.status(400).json({error : request.fileValidationError});
     } else {
         return response.status(201).json({success: true});
